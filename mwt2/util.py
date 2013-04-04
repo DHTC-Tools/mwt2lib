f15c2223add0db3ef93eda75f88372a7ea80ea04
David Champion <dgc@uchicago.edu>
2013-04-04 00:34:01 -0500

    Bring over cache and formatter classes from new backup program

--- mwt2/util.py
+++ mwt2/util.py
@@ -1,3 +1,4 @@
+import re
 
 
 def uniq(items):
@@ -48,3 +49,70 @@ class jsdict(dict):
 		return dict.__getitem__(self, key)
 
 
+class cache(dict):
+	'''A dictionary that acts as a cache to a lookup function.'''
+	def __init__(self, lookup):
+		self._lookup = lookup
+		dict.__init__(self)
+
+
+class formatter(object):
+	'''formatter is a generalized string-templating class.
+
+	A template contains variables expressed within {curly braces}.  When
+	a formatter is instantiated, the instance is a callable which accepts
+	a template as an argument and returns the formatted result.
+
+	At initialization, a formatter takes an arbitrary list of arguments,
+	which we term 'resolvers'.  Each resolver is used in turn to attempt
+	to resolve a template variable.  A resolver may be:
+	  * a callable, which takes the template variable as argument and
+	    returns the expansion of that variable;
+	  * a dictionary, which returns the result of 'resolver[variable]';
+	  * an object, which returns the result of 'resolver.variable';
+	  * a string or unicode string, which returns itself
+
+	Examples:
+	fmtr = formatter(lambda x: rot13(x))
+	fmtr('abc.{abc}') -> 'abc.nop'
+
+	x = { 'abc': 'alphabet' }
+	fmtr = formatter(x)
+	fmtr('{abc}') -> 'abc'
+	fmtr('{abc}.{xyz}') -> 'abc.'
+
+	x = { 'abc': 'alphabet' }
+	fmtr = formatter(x, 'nevermind')
+	fmtr('{abc}.{xyz}') -> 'abc.nevermind'
+
+	class X(object): pass
+	x = X()
+	x.foo = 'bar'
+	fmtr = formatter(x, rot13)
+	fmtr('{foo}.{xyz}') -> 'bar.klm'
+	'''
+
+	rx = re.compile('{([^}]+)}')
+
+	def __init__(self, *args):
+		self.resolvers = args
+
+	def __call__(self, s):
+		def resolver(match):
+			key = match.group(1)
+			for res in self.resolvers:
+				if callable(res):
+					return str(res(key))
+				if hasattr(res, '__contains__') and key in res:
+					return str(res[key])
+				if hasattr(res, key):
+					return str(getattr(res, key))
+				if type(res) in (type(''), type(u'')):
+					return res
+			return ''
+		return self.rx.sub(resolver, s)
+
+	def __getitem__(self, key):
+		if key not in self:
+			self[key] = self._lookup(key)
+		return dict.__getitem__(self, key)
