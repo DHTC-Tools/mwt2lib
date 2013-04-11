7286bf25a0e18b4324d8f10bb789da32a76d25a5
David Champion <dgc@bikeshed.us>
2013-04-11 13:42:41 -0500

    Add docstring to module as a whole.

--- mwt2/__init__.py
+++ mwt2/__init__.py
@@ -5,4 +5,17 @@ import syscmd
 import unbuffered
 import system
 
+'''mwt2lib is a Python library implementing a variety of tools,
+mechanics, utilities, and interfaces that are useful to the US ATLAS
+Midwest Tier 2 facility.  It is a work in progress, and not yet
+versioned apart from the changeset IDs in the versioning repositories.
+
+You can find mwt2lib here:
+https://github.com/DHTC-Tools/mwt2lib
+https://bitbucket.org/dhtc-tools/mwt2lib
+
+The Github repo is primary, but the two are synchronized at least
+daily.
+'''
+
 __all__ = [sym for sym in locals().keys() if not sym.startswith('_')]
